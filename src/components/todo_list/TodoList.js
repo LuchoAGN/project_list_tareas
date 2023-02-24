85b6cd997505806bd0e722b7ac67e541789d862d
Luis <garcialuis783@gmail.com>
2023-02-24 16:46:57 -0500

    add hide list

--- src/components/todo_list/TodoList.js
+++ src/components/todo_list/TodoList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './TodoList.scss'
+import { IoEyeSharp } from 'react-icons/io5'
 
 function TodoList(props){
     return(
-        <div className="todo-cmp ">
+        <div className="todo-cmp">
             {props.children}
             <div className="row">
-                <p></p>
+                <IoEyeSharp/>
+                <p>Ocultar las tareas completadas</p>
             </div>
         </div>
     )
